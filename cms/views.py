b9c84822b4ceadb56d39ba353a79b53e1b1aed7c
nshandra <n.shandra@alumnos.urjc.es>
2018-04-15 19:09:29 +0200

    Initial commit

--- cms/views.py
+++ cms/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse, HttpResponseNotFound
+from cms.models import Pages
 # Create your views here.
+
+
+def main(request):
+    resp = "Available pages: "
+    pages = Pages.objects.all()
+    for page in pages:
+        resp += "<br><a href=/" + page.name + ">" + page.name + "</a>"
+    return HttpResponse(resp)
+
+
+def get_page(request, requested_name):
+    try:
+        return HttpResponse(Pages.objects.get(name=requested_name).page)
+    except Pages.DoesNotExist:
+        return HttpResponseNotFound("<h1>Page does not exist.</h1>")
